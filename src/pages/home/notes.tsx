c4d5482a60e0f1cb9bef9806397f1d16e33dd356
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-25 22:00:24 +0900

    feat: 노트별 향수 추천의 향수 목록 부분 api 연결

--- src/pages/home/notes.tsx
+++ src/pages/home/notes.tsx
@@ -4,11 +4,51 @@ import {SectionSubTitle, SectionTitle} from './index.style'
 import styled from '@emotion/styled'
 import {Typography} from '@mui/material'
 import Category from '@components/category'
+import instance from '@api/instance'
+import {useQuery} from '@tanstack/react-query'
+
+type PerfumesType = {
+  content: {
+    id: number
+    name: string
+    thumbnail?: string
+    brandName: string
+    strength: string
+    duration: string
+  }[]
+  first: boolean
+  hasNext: boolean
+  last: boolean
+  pageNumber: number
+  size: number
+  totalElements: number
+  totalPages: number
+}
+
+const fetchGetPerfumesByCategories = async () => {
+  try {
+    // const res = await instance.get(`/perfumes/category/${id}?page=0&size=10`)
+    const res = await instance.get(`/perfumes/category/${1}?page=0&size=10`)
+    const data = res.data
+
+    return data
+  } catch (error: any) {
+    console.log(error)
+    throw error
+  }
+}
 
 const Notes = () => {
+  const [categoryId, setCategoryId] = useState<number>(1)
   const [clickedNote, setClickedNote] = useState<string>('fruit')
+  const {isLoading, error, data} = useQuery<PerfumesType>({
+    queryKey: ['perfumesByCategories'],
+    queryFn: () => fetchGetPerfumesByCategories(),
+  })
 
-  // 인라인스타일 주지말것.
+  console.log(categoryId - 1)
+  console.log(data)
+
   return (
     <Wrapper>
       <SectionTitle>노트별 향수 추천</SectionTitle>
@@ -19,47 +59,53 @@ const Notes = () => {
       <Category
         currentCategory={clickedNote}
         setCurrentCategory={setClickedNote}
+        setCategoryId={setCategoryId}
       />
 
-      <FlexBox style={{marginTop: 74, gap: 32}}>
-        <NoteBox>
-          <img
-            src="images/note_bg.png"
-            alt="note"
-            style={{
-              background:
-                'linear-gradient(0deg, rgba(0, 0, 0, 0.35) 0%, rgba(0, 0, 0, 0.35) 100%), lightgray 50% / cover no-repeat',
-              borderRadius: 16,
-              position: 'absolute',
-            }}
-          />
-          <NoteTitle>
-            {clickedNote.toUpperCase()}
-            <br />
-            NOTE
-          </NoteTitle>
-          <NoteSubTitle>
-            나무 향을 의미하며,
-            <br />
-            건조하고 성숙한 느낌을 전달합니다.
-          </NoteSubTitle>
-        </NoteBox>
-        <ProductLayout>
-          {new Array(6).fill(0).map((_, idx) => (
-            <ProductBox key={idx}>
-              <img
-                src="images/perfume_test.png"
-                alt="product"
-                style={{objectFit: 'contain', width: '100%', height: 206}}
-              />
-              <ProductInfoBox>
-                <ProductName>오드퍼퓸 플레르드뽀 750ml</ProductName>
-                <BrandName>딥디크</BrandName>
-              </ProductInfoBox>
-            </ProductBox>
-          ))}
-        </ProductLayout>
-      </FlexBox>
+      {!isLoading && !error && (
+        <FlexBox style={{marginTop: 74, gap: 32}}>
+          <NoteBox>
+            <img
+              src="images/note_bg.png"
+              alt="note"
+              style={{
+                background:
+                  'linear-gradient(0deg, rgba(0, 0, 0, 0.35) 0%, rgba(0, 0, 0, 0.35) 100%), lightgray 50% / cover no-repeat',
+                borderRadius: 16,
+                position: 'absolute',
+              }}
+            />
+            <NoteTitle>
+              {clickedNote.toUpperCase()}
+              <br />
+              NOTE
+            </NoteTitle>
+            <NoteSubTitle>
+              나무 향을 의미하며,
+              <br />
+              건조하고 성숙한 느낌을 전달합니다.
+            </NoteSubTitle>
+          </NoteBox>
+          <ProductLayout>
+            {data!.content.map((perfume, idx) => (
+              <ProductBox key={idx}>
+                <ThumbNail
+                  src={
+                    perfume.thumbnail
+                      ? perfume.thumbnail
+                      : 'images/perfume_test.png'
+                  }
+                  alt="product"
+                />
+                <ProductInfoBox>
+                  <ProductName>{perfume.name}</ProductName>
+                  <BrandName>{perfume.brandName}</BrandName>
+                </ProductInfoBox>
+              </ProductBox>
+            ))}
+          </ProductLayout>
+        </FlexBox>
+      )}
     </Wrapper>
   )
 }
@@ -134,4 +180,10 @@ const ProductName = styled(Typography)({
 const BrandName = styled(Typography)({
   color: '#A9A9A9',
   fontSize: 14,
+})
+
+const ThumbNail = styled.img({
+  objectFit: 'contain',
+  width: '100%',
+  height: '75%',
 })
