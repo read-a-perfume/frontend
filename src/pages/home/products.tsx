7748845039cc9f54d318788acd95c68ea8511a99
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-29 01:31:46 +0900

    feat: 사람들이 많이 찾은 향수에 pagination 추가

--- src/pages/home/products.tsx
+++ src/pages/home/products.tsx
@@ -1,12 +1,20 @@
 import {SectionSubTitle, SectionTitle} from './index.style.js'
 import styled from '@emotion/styled'
 import FlexBox from '@layouts/flex-box.js'
-import {Typography} from '@mui/material'
+import {Pagination, PaginationItem, Typography} from '@mui/material'
 import PerfumeCharacteristics from './perfume-characteristics.js'
 import {useNavigate} from 'react-router-dom'
+import {useState} from 'react'
+
+const products = new Array(6).fill(0).map((_, i) => i + 1)
 
 const Products = () => {
   const navigate = useNavigate()
+  const [page, setPage] = useState<number>(0)
+
+  const handlePage = (_, page: number) => {
+    setPage(page)
+  }
 
   return (
     <div style={{marginTop: 136}}>
@@ -15,7 +23,7 @@ const Products = () => {
         사람들이 많이 검색한 향수 위주로 모아봤어요
       </SectionSubTitle>
       <ProductBox>
-        {new Array(6).fill(0).map((_, idx) => (
+        {products.map((_, idx) => (
           <FlexBox
             key={idx}
             direction="column"
@@ -35,6 +43,25 @@ const Products = () => {
           </FlexBox>
         ))}
       </ProductBox>
+      <FlexBox justifyContent="center">
+        <Pagination
+          page={page}
+          count={Math.ceil(products.length / 6)}
+          onChange={handlePage}
+          variant="outlined"
+          shape="rounded"
+          size="large"
+          hidePrevButton
+          hideNextButton
+          renderItem={item => <Item {...item} />}
+          sx={{
+            marginTop: 30,
+            '& .Mui-selected': {
+              backgroundColor: '#D9D9D9',
+            },
+          }}
+        />
+      </FlexBox>
     </div>
   )
 }
@@ -44,7 +71,7 @@ export default Products
 const ProductBox = styled.div({
   display: 'grid',
   gridTemplateColumns: 'repeat(4, 1fr)',
-  gridTemplateRows: 'repeat(3, 1fr)',
+  gridTemplateRows: 'repeat(2, 1fr)',
   rowGap: '88px',
 })
 
@@ -78,4 +105,13 @@ const ProductName = styled(Typography)({
   fontSize: 18,
   fontWeight: '600',
   color: '#131313',
+})
+
+const Item = styled(PaginationItem)({
+  backgroundColor: '#F1F1F5',
+  outline: 'none',
+  border: 'none',
+  fontSize: 16,
+  fontWeight: '500',
+  marginRight: 10,
 })
