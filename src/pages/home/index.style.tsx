1d19317245dcc903cc83ad6f1efc92b2314519c2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2023-11-29 13:49:03 +0900

    fix: 기본 색상 red 삭제

--- src/pages/home/index.style.tsx
+++ src/pages/home/index.style.tsx
@@ -4,7 +4,6 @@ import {Typography} from '@mui/material'
 export const Banner = styled.div({
   width: '100%',
   height: '470px',
-  background: 'red',
 })
 
 export const BannerImage = styled.img({
