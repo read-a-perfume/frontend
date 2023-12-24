cd7d2962c843db8784244a3e344410001110404a
nayounsang <nayounsang722@gmail.com>
2023-12-24 15:14:04 +0900

    refactor: 향수와 유저 querykey작성

---
+++ src/react-query-keys/user.keys.ts
@@ -0,0 +1,13 @@
+export const userQueryKeys = {
+  me: ['me'],
+  userTastes: ['user-tastes'],
+  mypageReviews: ['mypage-reviews'],
+}
+
+export const followQueryKeys = {
+  follows: ['follows'],
+}
+
+export const followMutateKeys = {
+  mypageFollow: ['mypage-follow'],
+}
