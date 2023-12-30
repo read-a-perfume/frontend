5f991104084eedb0ab799b5a87f6e083c6ae54f1
jihun <gn753@naver.com>
2023-12-30 19:16:24 +0900

    feat: 새로고침 및 로그아웃 추가

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {fetchRefreshToken} from 'src/store/server/auth/queries'
 
 axios.defaults.withCredentials = true
 const instance = axios.create({
@@ -12,3 +13,19 @@ const instance = axios.create({
 })
 
 export default instance
+instance.interceptors.response.use(
+  response => {
+    return response
+  },
+  async function refresh(error) {
+    const originalRequest = error.config
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+      const response = await fetchRefreshToken()
+      if (response?.status === 200) {
+        return instance(originalRequest)
+      }
+    }
+    return Promise.reject(error)
+  },
+)
--- src/pages/reviews/review-list/index.tsx
+++ src/pages/reviews/review-list/index.tsx
@@ -7,8 +7,8 @@ const ListSection = lazy(() => import('./list-section'))
 const skeletons = Array.from({length: 10}, (_, index) => index + 1)
 const ReviewListPage = () => {
   const [sort, setSort] = useState<'RECENT' | 'LIKE'>('RECENT')
-  const handleChangeSort = e => {
-    setSort(e.target.value)
+  const handleChangeSort = tab => {
+    setSort(tab)
   }
 
   return (
@@ -18,6 +18,7 @@ const ReviewListPage = () => {
           sectionTitle="향수 리뷰"
           buttonText="리뷰 작성하기"
           optionName={['최신순', '좋아요순']}
+          sort={sort}
           handleChangeSort={handleChangeSort}
         />
         <Suspense fallback={<ReviewSkeleton skeletons={skeletons} />}>
