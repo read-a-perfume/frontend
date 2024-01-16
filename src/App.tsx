7b74ab41b8dd5bee461f3571a3278a335f2cff9e
nayounsang <nayounsang722@gmail.com>
2024-01-16 23:27:38 +0900

    fix:suspense fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import {RouterProvider} from 'react-router-dom'
 import {router} from './router'
+import { Box } from '@mui/material'
+import { Suspense } from 'react'
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <Suspense fallback={<Box width="100%" height="100vh" />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  )
 }
 
 export default App
