a30191767ba97179586f39c62038f498362c3526
jihun <gn753@naver.com>
2024-01-18 14:12:00 +0900

    chore: import  패키지 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,23 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {browser: true, es2020: true},
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'mui-path-imports'],
   //eslint 세부 규칙 설정
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': [
+      'warn',
+      {allowConstantExport: true},
     ],
-    "@typescript-eslint/no-explicit-any": 0, //any허용
-    "no-useless-escape": 0, // \ 허용
+    '@typescript-eslint/no-explicit-any': 0, //any허용
+    'no-useless-escape': 0, // \ 허용
+    // "mui-path-imports/mui-path-imports": "error"
   },
-};
+}
