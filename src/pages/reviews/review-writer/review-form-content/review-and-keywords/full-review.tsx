4c23253e1256bbe7aa1e9ab27d630f4a4da9983f
jihun <gn753@naver.com>
2024-01-24 20:36:57 +0900

    refector: 리뷰 작성 폼 useForm 리팩토링

--- src/pages/reviews/review-writer/review-form-content/review-and-keywords/full-review.tsx
+++ src/pages/reviews/review-writer/review-form-content/review-and-keywords/full-review.tsx
@@ -22,12 +22,10 @@ const FullReview = ({title}: {title: string}) => {
       </Typography>
       <AutoSizer
         aria-label="minimum height"
-        name={field.name}
-        required={false}
+        {...field}
+        required={true}
         minRows={3}
         maxLength={200}
-        value={field.value}
-        onChange={field.onChange}
         placeholder="이 향수를 사용하면서 느낀 특징과 매력을 설명해주세요. 특유의 향과 향수의 노트들에 대해 느낀 점이나 어떤 면이 인상적이었는지에 대해 언급해주시면 좋습니다."
       />
       <ErrorMessage errorMessage={errors.fullReview?.message} />
