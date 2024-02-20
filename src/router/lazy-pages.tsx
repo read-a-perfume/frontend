53b46520272998845b5299cd255b416d55c1b9cb
jihun <gn753@naver.com>
2024-02-20 17:27:01 +0900

    test: 성능 테스트 용 브랜치

--- src/router/lazy-pages.tsx
+++ src/router/lazy-pages.tsx
@@ -1,15 +1,21 @@
-export const LazySignUpPage = import('@pages/sign-up')
-export const LazySignInPage = import('@pages/sign-in')
+import {lazy} from 'react'
 
-export const LazyReviewListPage = import('@pages/reviews/review-list')
+export const LazySignUpPage = lazy(() => import('@pages/sign-up'))
+export const LazySignInPage = lazy(() => import('@pages/sign-in'))
 
-export const LazyReviewWriterPage = import('@pages/reviews/review-writer')
-
-export const LazyPerfumesPage = import('@pages/perfumes/index')
-export const LazyPerfumeDetailsPage = import('@pages/perfume-detail')
-
-export const LazyBrandDetailsPage = () => import('@pages/brand/brand-page')
-
-export const LazyBrandsPage = import('@pages/brand/brand-list')
-export const LazyMyPage = import('@pages/my-page')
-export const LazyAccountPage = import('@pages/account')
+export const LazyReviewListPage = lazy(
+  () => import('@pages/reviews/review-list'),
+)
+export const LazyReviewWriterPage = lazy(
+  () => import('@pages/reviews/review-writer'),
+)
+export const LazyPerfumesPage = lazy(() => import('@pages/perfumes/index'))
+export const LazyPerfumeDetailsPage = lazy(
+  () => import('@pages/perfume-detail'),
+)
+export const LazyBrandDetailsPage = lazy(
+  () => import('@pages/brand/brand-page'),
+)
+export const LazyBrandsPage = lazy(() => import('@pages/brand/brand-list'))
+export const LazyMyPage = lazy(() => import('@pages/my-page'))
+export const LazyAccountPage = lazy(() => import('@pages/account'))
