51aca1e2c7135aa680ad066526a42d9bd89734d0
Jihun Go <71584114+gn753@users.noreply.github.com>
2024-02-20 17:17:20 +0900

    test: 성능 테스트

--- src/router/lazy-pages.tsx
+++ src/router/lazy-pages.tsx
@@ -1,21 +1,15 @@
-import {lazy} from 'react'
+export const LazySignUpPage = import('@pages/sign-up')
+export const LazySignInPage = import('@pages/sign-in')
 
-export const LazySignUpPage = lazy(() => import('@pages/sign-up'))
-export const LazySignInPage = lazy(() => import('@pages/sign-in'))
+export const LazyReviewListPage = import('@pages/reviews/review-list')
 
-export const LazyReviewListPage = lazy(
-  () => import('@pages/reviews/review-list'),
-)
-export const LazyReviewWriterPage = lazy(
-  () => import('@pages/reviews/review-writer'),
-)
-export const LazyPerfumesPage = lazy(() => import('@pages/perfumes/index'))
-export const LazyPerfumeDetailsPage = lazy(
-  () => import('@pages/perfume-detail'),
-)
-export const LazyBrandDetailsPage = lazy(
-  () => import('@pages/brand/brand-page'),
-)
-export const LazyBrandsPage = lazy(() => import('@pages/brand/brand-list'))
-export const LazyMyPage = lazy(() => import('@pages/my-page'))
-export const LazyAccountPage = lazy(() => import('@pages/account'))
+export const LazyReviewWriterPage = import('@pages/reviews/review-writer')
+
+export const LazyPerfumesPage = import('@pages/perfumes/index')
+export const LazyPerfumeDetailsPage = import('@pages/perfume-detail')
+
+export const LazyBrandDetailsPage = () => import('@pages/brand/brand-page')
+
+export const LazyBrandsPage = import('@pages/brand/brand-list')
+export const LazyMyPage = import('@pages/my-page')
+export const LazyAccountPage = import('@pages/account')
